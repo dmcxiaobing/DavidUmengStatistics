b745cae46b5b935bf8c11c6de5b1db5200551199
Administrator <admin>
2016-09-21 10:14:43 +0800

    提交umeng统计app应用Demo

---
+++ app/src/main/java/davidumengstatistics/qq986945193/davidumengstatistics/davidumengstatistics/MainActivity.java
@@ -0,0 +1,25 @@
+package davidumengstatistics.qq986945193.davidumengstatistics.davidumengstatistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.umeng.analytics.MobclickAgent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+}
